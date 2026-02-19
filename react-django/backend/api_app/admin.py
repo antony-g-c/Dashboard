12287c203d33324fa00c39d2889d714c284a168b
antony-g-c <antony.g.chackieth@gmail.com>
2026-02-19 16:19:21 -0500

    Remove unrelated prototype projects and virtualenvs

--- react-django/backend/api_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from api_app.models import Patient
-
-# Register your models here.
-admin.site.register(Patient)
--- react-django/backend/api_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from api_app.models import Patient
-
-class PatientSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Patient
-        fields = ['patient_id','first_name','last_name','blood']
